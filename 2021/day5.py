a3f3545fe26e757030e514e45ba6bd4f725fdc7a
Steven Whitaker <sjwhitak@mtu.edu>
2021-12-11 10:35:25 -0500

    day5part2

--- 2021/day5.py
+++ 2021/day5.py
@@ -17,7 +17,7 @@ pipes = parse_day5(file)
 # straight lines
 sl = np.logical_or( (pipes[:,0] == pipes[:,2]), (pipes[:,1] == pipes[:,3]) )
 
-pipes = pipes[sl]
+straight_pipes = pipes[sl]
 
 # Initialize ocean floor
 maxx, minx = np.max(pipes[:,(0,2)]), np.min(pipes[:,(0,2)])
@@ -25,9 +25,9 @@ maxy, miny = np.max(pipes[:,(1,3)]), np.min(pipes[:,(1,3)])
 
 floor = np.zeros((maxx-minx+1, maxy-miny+1))
 
-# Run through each pipe
-for pipe in pipes:
-    
+# Part 1 ---------------------------------------------------------------------
+# Run through straight pipes
+for pipe in straight_pipes:
     if pipe[0] < pipe[2]:
         dir_x = 1
     else:
@@ -51,4 +51,52 @@ for pipe in pipes:
             floor[pipe[0]-minx, pipe[1]+i*dir_y-miny] += 1
     
 print(np.sum( floor >= 2 ))
+
+# Part 2 ---------------------------------------------------------------------
+# Run through all pipes
+floor = np.zeros((maxx-minx+1, maxy-miny+1))
+for straight,pipe in zip(sl,pipes):
     
+    if straight:
+        
+        if pipe[0] < pipe[2]:
+            dir_x = 1
+        else:
+            dir_x = -1
+        
+        if pipe[1] < pipe[3]:
+            dir_y = 1
+        else:
+            dir_y = -1
+        
+        
+        length_x = abs(pipe[0] - pipe[2]) + 1
+        length_y = abs(pipe[1] - pipe[3]) + 1
+        
+        # for loop to iterate over length of pipe
+        if length_x > 1:
+            for i in range(length_x):
+                floor[pipe[0]+i*dir_x-minx, pipe[1]-miny] += 1
+        if length_y > 1:
+            for i in range(length_y):
+                floor[pipe[0]-minx, pipe[1]+i*dir_y-miny] += 1
+        
+    # Diagonal lines
+    else:
+        if pipe[0] < pipe[2]:
+            dir_x = 1
+        else:
+            dir_x = -1
+        
+        if pipe[1] < pipe[3]:
+            dir_y = 1
+        else:
+            dir_y = -1
+        
+        
+        length_x = abs(pipe[0] - pipe[2]) + 1
+        length_y = abs(pipe[1] - pipe[3]) + 1     
+        
+        for i in range(length_x):
+            floor[pipe[0]+i*dir_x-minx, pipe[1]+i*dir_y-miny] += 1
+print(np.sum( floor >= 2 ))

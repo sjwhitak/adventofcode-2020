a52ecd5780c42ddfe4523489bb4a714ed8eaad1b
Steven Whitaker <sjwhitak@mtu.edu>
2021-12-11 18:08:06 -0500

    day9part2

--- 2021/day9.py
+++ 2021/day9.py
@@ -23,8 +23,62 @@ for x in range(x_max):
             local_minima.append(floor[x,y]+1)
 print( np.sum(local_minima) )
 
-# Part 2 is looking for boundaries of 9's, then counting the area 
-# surrounding the 9's.
+# Part 2 -- map out sizes of surrounding contours with 9 being boundary
 floor_mask = floor != 9
-local_minima = list()
-# Run through the tiles 
+
+# I cheat and know that (0,0) will start on a basin
+x = 0
+y = 0
+
+
+# Find one basin
+valid_positions = [(0,0)]
+previous_positions = []
+count = 0
+
+def basin_count(mask, position):
+    valid_positions = [position]
+    x_max = len(mask[:,0])
+    y_max = len(mask[0,:])
+    count = 1
+    
+    mask[position[0], position[1]] = False
+    
+    while True:
+        # End when you're done with all available positions to search
+        if len(valid_positions) == 0:
+            return count, mask
+        
+        # Start at most recent position
+        x, y = valid_positions[0]
+        
+
+        
+        # Look at 4 surrounding areas
+        for dx, dy in adjacents:
+            if x+dx >= 0 and x+dx < x_max and y+dy >= 0 and y+dy < y_max:
+                
+                # Is it a possible area?
+                if mask[x+dx, y+dy]:
+
+                            
+                    # It's a valid position! Save it and remove from search
+                    # capabilities.
+                    mask[x+dx, y+dy] = False
+                    count += 1
+                    valid_positions.append((x+dx, y+dy))
+                            
+                                 
+        # Move to the next position
+        valid_positions.pop(0)
+
+# Search each position for a basin
+basin_sizes = list()
+for x in range(x_max):
+    for y in range(y_max):
+        if floor_mask[x,y]:
+            size, floor_mask = basin_count(floor_mask, (x,y))
+            basin_sizes.append(size)
+
+max_3 = sorted(basin_sizes)[-3:]
+print(max_3[0] * max_3[1] * max_3[2])
